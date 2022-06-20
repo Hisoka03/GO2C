624de4467bab5ce1c10089ee7b2756f6fc6e214f
Hisoka03 <Hisoka03Kh@github.com>
2022-06-20 23:25:20 +0100

    add

---
+++ assets/js/popup.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+
+    $("#s1ComMan").click(function () { 
+        
+
+        Swal.fire({
+            title: 'Community Management',
+            text:'Votre communication sur les réseaux sociaux ne s’improvise pas. Vous êtes appelés à créer des contenus attrayants, gérer les publications et animer votre communauté. Votre stratégie digitale doit être établie et mise en œuvre tout en tenant compte de votre image de marque et de la nature de vote activité et de votre clientèle cible. À GO 2C, nous vous proposons des solutions de Social Media sur mesure, parfaitement adaptées à vos besoins « marketing » et « Métier ». Confiez votre présence sur les réseaux sociaux à nos experts !',
+            confirmButtonColor: '#D4AF37',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        
+    });
+
+    
+    $("#s2DesGra").click(function () { 
+        
+
+        Swal.fire({
+            title: 'Design Graphique',
+            text:"Vous avez besoin d'un logo ? D’une charte graphique ? ou peut-être d'un rebranding complet de votre marque ? GO 2C votre agence de Design Graphique vous fournit des conceptions personnalisées, créatives, et de haute qualité que ce soit pour des supports numériques ou bien pour des supports imprimables",
+            confirmButtonColor: '#D4AF37',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        
+    });
+
+    $("#s3").click(function () { 
+        
+
+        Swal.fire({
+            title: 'Marketing de contenu',
+            text:"Si vous désirez que votre site internet soit visible auprès de prospects et clients potentiels, il ne faut pas vous stopper uniquement à un bon développement et design de votre site. En effet, le contenu de votre site internet est un élément essentiel à prendre en compte et c’est pour cela que les spécialistes de notre agence de contenu web composerons pour vous un texte de qualité, attractif et clair, vous permettant ainsi de conquérir, rasséréner et persuader vos utilisateurs de manière à les transformer en clients potentiels.",
+            confirmButtonColor: '#D4AF37',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        
+    });
+
+
+    $("#s4").click(function () { 
+        
+
+        Swal.fire({
+            title: 'E-mail marketing',
+            text:"GO 2C vous assure des campagnes d’e-mailing qualitatives. Nous mettons en place une stratégie d’e-mail marketing, en vous proposant un design et un contenu personnalisé, destinés à votre cible.",
+            confirmButtonColor: '#D4AF37',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        
+    });
+
+
+    $("#s5").click(function () { 
+        
+
+        Swal.fire({
+            title: 'Stratégie Marketing',
+            text:"GO 2C, votre agence de stratégie digitale à Tunis, vous assiste étroitement dans la construction de votre stratégie de marketing digital afin d’optimiser votre ROI de la façon la plus efficiente possible.",
+            confirmButtonColor: '#D4AF37',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        
+    });
+
+
+    
+
+})
+
+
